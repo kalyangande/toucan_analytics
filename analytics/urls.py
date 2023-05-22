6c3ac3debf7248562ddf60080063d451b04de2f1
Your Name <you@example.com>
2023-05-22 23:00:45 +0530

    Changed urls.py and viwes.py as required

--- analytics/urls.py
+++ analytics/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('frequent-mode-of-payments',frequent_mode_of_payments),
-    path('payments-per-interval',payments_per_interval),
-    path('emi-payments',emi_payments),
-    path('spending-sectors',spending_sectors),
+    path('analytics',analytics),
     path('',index)
 ]
--- analytics/views.py
+++ analytics/views.py
@@ -1,16 +1,7 @@
 from django.shortcuts import render,HttpResponse
 
 # Create your views here.
-def frequent_mode_of_payments(request):
-    pass
-
-def payments_per_interval(request):
-    pass
-
-def emi_payments(request):
-    pass
-
-def spending_sectors(request):
+def analytics(request):
     pass
 
 def index(request):
