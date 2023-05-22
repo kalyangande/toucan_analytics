47f9cfc62a09dfbf973ff1c2c5a2e90614785c73
Your Name <you@example.com>
2023-05-22 14:25:57 +0530

    added placeholder for writing code in views.py.

--- analytics/views.py
+++ analytics/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+def frequent_mode_of_payments(request):
+    pass
+
+def payments_per_interval(request):
+    pass
+
+def emi_payments(request):
+    pass
+
+def spending_sectors(request):
+    pass
+
