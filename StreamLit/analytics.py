f2042e6473fa704c226eb9c935d97d0bad959ee5
GopiKrishna2934 <gopi.chidarapu@toucanus.com>
2023-05-24 22:43:18 +0530

    Send a sample post and get request to django using streamlit

--- StreamLit/analytics.py
+++ StreamLit/analytics.py
@@ -1,14 +1,46 @@
 import streamlit as st
 import matplotlib.pyplot as plt
+import requests
+import json
+import streamlit as st
+from django.http import request
+import streamlit as st
 
-# Pie chart, where the slices will be ordered and plotted counter-clockwise:
-labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
-sizes = [15, 30, 45, 10]
-explode = (0, 0, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+col1, col2= st.columns(2)
 
-fig1, ax1 = plt.subplots()
-ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
-         startangle=90)
-ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+data = {
+            "title": "Streamlit Request",
+            "body": "This is a sample request",
+            "userId": 1
+        }
+with col1:
+    if st.button('Post'):
+        response_post = requests.post("http://127.0.0.1:8000/analytics", data=data)
+        if response_post.status_code == 200:
+            st.write("POST request successful!")
+            st.write(data)
+        else:
+            st.write(f"POST request failed! {response_post.status_code}")
+    else:
+        st.write('click here')
 
-st.pyplot(fig1)
+with col2:
+    a=st.button
+    if a('get'):
+        response_get = requests.get("http://127.0.0.1:8000/analytics")
+        if response_get.status_code == 200:
+            st.write("GET request successful!")
+            st.write("Response:")
+            st.write(response_get.json())
+            
+            
+        else:
+            st.write(f"GET request failed! {response_get.status_code}")
+    else:
+        st.write('click here')
+
+
+
+
+
+
--- analytics/views.py
+++ analytics/views.py
@@ -1,8 +1,41 @@
 from django.shortcuts import render,HttpResponse
+from django.http import JsonResponse
+import requests
+import json
+from django.views.decorators.csrf import csrf_exempt
+
+import csv
+
 
 # Create your views here.
+@csrf_exempt
 def analytics(request):
-    pass
+    
+    if request.method == "GET":
+        file = open('/home/gopikrishna/Taigo/analytics_env/toucan_analytics/Data/data_for_database.csv',mode='r')
+        data = csv.reader(file)
+        dL = list(data)
+        dataList = dL[1:]
+        for row in dataList:
+            customerid = row[1]
+            category = row[2]
+            modeOfPayment = row[3]
+            amount = row[4]
+            date = row[5]
+            break
+
+        dictt = {
+            
+            "customerid" : customerid,
+            "category" : category,
+            "mode" : modeOfPayment,
+            "amount" : amount,
+            "date" : date
+        }
+        file.close()
+        return JsonResponse(dictt)
+    
+    return HttpResponse("wow")
 
 def index(request):
     return HttpResponse("index")
