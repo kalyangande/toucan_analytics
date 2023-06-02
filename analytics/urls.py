da43422315b515f280ad24e54b84158870aff607
pentamharithasri <130429648+pentamharithasri@users.noreply.github.com>
2023-06-02 17:44:04 +0530

    Update urls.py

--- analytics/urls.py
+++ analytics/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import *
-from analytics import views
+from django.urls import path
+from .views import Analytics,DataView,index
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+
 urlpatterns = [
-    path('data/', views.HelloView.as_view(), name='hello'),
-    path('analytics/',analytics),
+    path('analytics/',Analytics.as_view()),
     path('',index),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('data/', DataView.as_view(), name='data'),
 ]
